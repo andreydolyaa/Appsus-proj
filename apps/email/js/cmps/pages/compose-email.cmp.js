1ab7c49c010f5e5a90d64db2d017ea02c31e2e90
harelwn <harelwn@users.noreply.github.com>
2020-11-05 00:47:27 +0200

    v01

--- apps/email/js/cmps/pages/compose-email.cmp.js
+++ apps/email/js/cmps/pages/compose-email.cmp.js
@@ -3,23 +3,34 @@ export default {
     name:'email-compose',
     template:`
         <section class="email-compose">
-          <h2>New Messege</h2>
+          <div class="header">
+            <h2>New Messege</h2>
+            <button class="close">X</button>
+          </div>
           <div class="formContainer">
               <form>
                     <div class="rub">
-                    <label>From:</label><input class="input" v-model="newEmail.from" placeholder=""/>
+                        <label>From:</label>
+                        <input class="input" v-model="newEmail.from" placeholder=""/>
                     </div>
 
                     <div class="rub">
-                    <label>To:</label><input class="input" v-model="newEmail.from" placeholder=""/>
+                        <label>To:</label>
+                        <input class="input" v-model="newEmail.from" placeholder=""/>
                     </div>
 
                     <div class="rub">
-                    <label>Subject:</label><input class="input" v-model="newEmail.subject" placeholder=""/>
+                        <label>Subject:</label>
+                        <input class="input" v-model="newEmail.subject" placeholder=""/>
                     </div>
 
                     <div class="rub">
-                    <label>Body:</label><textarea class="input" v-model="newEmail.body" placeholder=""></textarea>
+                        <label>Body:</label>
+                        <textarea class="input" v-model="newEmail.body" placeholder=""></textarea>
+                    </div>
+
+                    <div class="rub">
+                        <button class="sendBtn">Send</button>
                     </div>
               </form>
               <!-- {{newEmail}} -->

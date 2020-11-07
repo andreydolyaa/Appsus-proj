07f2b35d5195c4c3f3058fe8ca64715c7a5ed138
Andrey Dolya <dolya7kk@gmail.com>
2020-11-07 23:28:47 +0200

    sfsdf

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -4,17 +4,17 @@ export default {
 
         <nav>
             <router-link class="mainlink LinkGmail" to="/email/inbox" exact>
-                <img class="logo" src="assets/img/Gmail-logo.png"/>
+                <img class="logo" src="assets/img/email.png"/>
                 <div class="name">Email</div>
             </router-link>
             
             <router-link class="mainlink LinkKeeps" to="/keeps" exact>
-                <img class="logo" src="assets/img/keeps-logo.jpg"/>
+                <img class="logo" src="assets/img/post.png"/>
                 <div class="name">Keeps</div>
             </router-link>
 
             <router-link class="LinkKeeps" to="/books" exact>
-                <img class="logo books-logo" src="assets/img/books-logo.png"/>
+                <img class="logo books-logo" src="assets/img/study.png"/>
                 <div class="name">Books</div>
             </router-link>
              
